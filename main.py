2618ce9935cfef68af0a8474855cabe5a4292f72
holubka11 <sashkogolubka@gmail.com>
2026-03-14 23:25:06 +0200

    feature-branch commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import asyncio
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 
-# У реальному проекті токен береться з .env
+
 API_TOKEN = 'YOUR_BOT_TOKEN_HERE'
 
 bot = Bot(token=API_TOKEN)
@@ -12,9 +12,3 @@ dp = Dispatcher()
 async def cmd_start(message: types.Message):
     await message.answer("Вітаю у SmartHunt! Я допоможу тобі знайти фріланс-проєкти.")
 
-async def main():
-    print("Бот SmartHunt запущений...")
-    await dp.start_polling(bot)
-
-if __name__ == "__main__":
-    asyncio.run(main())
